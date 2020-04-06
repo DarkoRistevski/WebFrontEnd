98bfec019fa356cd8103853737f89cf66963ecbf
Darko Ristevski <darkoristevski97@gmail.com>
2020-04-06 13:49:12 +0200

    public/body

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import logo3 from'../../images/logo3.png'
+import recipe1 from'../../images/recipe1.jpg'
+import recipe3 from'../../images/recipe3.jpeg'
+import recipe4 from'../../images/recipe4.jpeg'
+
+function App() {
+  return (
+
+          <div className="ok">
+
+              <div className="s">
+                  ok
+              </div>
+
+              <div>
+                  ok
+              </div>
+
+          </div>
+
+
+      );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
